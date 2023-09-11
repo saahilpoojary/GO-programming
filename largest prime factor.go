b30d1bfa64d893c059ad9c9fc97093d5af6a7622
saahilpoojary <136162151+saahilpoojary@users.noreply.github.com>
2023-09-11 10:21:15 +0530

    largest prime factor.go

--- largest prime factor.go
+++ largest prime factor.go
@@ -1,32 +1,24 @@
-
 package main
+
 import (
 	"fmt"
-	"math"
 )
-func largestPrimFactor(n int) int {
-	largest := 1
 
-	for n % 2 == 0 {
-		largest = 2
-		n = n/2
-	}
+func largestPrimeFactor(n int) int {
+	largest := -1
 
-	for i := 3; i <= int(math.Sqrt(float64(n))); i += 2 {
-		for n % i == 0 {
+	for i := 2; i <= n; i++ {
+		for n%i == 0 {
 			largest = i
-			n = n/i
+			n /= i
 		}
-	}
-
-	if n > 2 {
-		largest = n
 	}
 
 	return largest
 }
 
 func main() {
-	number := 45
-	fmt.Println("The largest prime factor of", number, "is", largestPrimFactor(number))
+	num := 38 // Replace this with the number for which you want to find the largest prime factor
+	result := largestPrimeFactor(num)
+	fmt.Printf("The largest prime factor of %d is %d\n", num, result)
 }
